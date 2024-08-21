b64a48631ed9a93ba17fc720502ca727ed918970
Предраг Николић <idmpepe@gmail.com>
2024-08-21 15:22:52 +0200

    remove

--- libs/lsp/server.py
+++ libs/lsp/server.py
@@ -1,5 +1,5 @@
 from __future__ import annotations
-from typing import  Any, Dict, Generic, List, Optional, TypeVar, Union, override
+from typing import  Any, Dict, Generic, List, Optional, TypeVar, Union
 import asyncio
 import json
 
