ea3f1c097da0c71ee82a61e84c180808a35e138f
Предраг Николић <idmpepe@gmail.com>
2025-06-07 22:19:22 +0200

    remove try

--- main.py
+++ main.py
@@ -4,8 +4,6 @@ from .libs.lsp.manage_servers import ManageServers
 from .libs.lsp.text_change_listener import MirTextChangeListener
 from .libs.lsp.providers import callbacks_when_ready
 from .libs.lsp.server import server_callbacks_when_ready
-from Mir import mir_logger
-mir_logger.debug('hello')
 
 def plugin_loaded():
     [cb() for cb in callbacks_when_ready]
